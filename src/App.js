0095a2416019238921758f494ddc61226f7ea163
taewan <taewan2002@naver.com>
2023-04-11 23:30:04 +0900

    update

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
   const [charCount, setCharCount] = useState(0);
+  const [phoneNumber, setPhoneNumber] = useState('');
 
   const captureRef = useRef(null);
 
@@ -37,6 +38,20 @@ function App() {
   };
 
   const isDisabled = dream.length < 5 || dream.length > 100;
+
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+  };
+
+  const handleSendPhoneNumber = async () => {
+    try {
+      await axios.post('/api/gpt/test', { phoneNumber, dreamName: dream });
+      alert('전화번호가 성공적으로 전송되었습니다.');
+    } catch (error) {
+      console.error(error);
+      alert('전화번호 전송 중 오류가 발생했습니다. 다시 시도해주세요.');
+    }
+  };
 
   const handleDreamChange = (e) => {
     setDream(e.target.value);
@@ -82,10 +97,25 @@ function App() {
                 <p style={{ textAlign: 'justify', margin: '2%' }}>{result.dream_resolution}</p>
                 <p style={{ textAlign: 'justify', margin: '2%' }}>{result.today_luck}</p>
               </div>
+              <div style={{ margin: '2%' }}>
+                <p>안녕하세요!! 저희는 가천 코코네스쿨 2기 도슨트 팀 입니다. 서비스 출시에 앞서 테스트하기 위해 만들어진 페이지 입니다. 서비스 출시 소식이 궁금하시면 전화번호를 남겨주세요.</p>
+                <label htmlFor="phone-number-input">전화번호를 입력해보세요:</label>
+                <input
+                    id="phone-number-input"
+                    type="tel"
+                    pattern="\d{11}"
+                    value={phoneNumber}
+                    onChange={handlePhoneNumberChange}
+                    required
+                />
+                <button onClick={handleSendPhoneNumber} disabled={!phoneNumber || phoneNumber.length < 10 || phoneNumber.length > 11}>
+                  전화번호 전송
+                </button>
+              </div>
+
             </>
         )}
       </div>
   );
 }
-
 export default App;

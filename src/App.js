e3e4b5d0f69cee34a2f6d433c39a74d3c83cb4dd
taewan <taewan2002@naver.com>
2023-04-13 05:19:52 +0900

    update

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     }
     setLoading(true);
     try {
-      const response = await axios.post(`/api/gpt`, {
+      const response = await axios.post(`/api/gpt/survey`, {
         dream,
         gender,
         age,

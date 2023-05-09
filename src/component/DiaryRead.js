8a9732b7069faa45fd1d24af33b89ea01572b80d
Junbeetime <sgwag597@gmail.com>
2023-05-09 21:21:01 +0900

    feat: 꿈 로딩이팩트 추가, 편집 기능 추가

--- src/component/DiaryRead.js
+++ src/component/DiaryRead.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getDiary, updatePublicStatus } from "../services/apiService";
+import { getDiary, updatePublicStatus, editDiary } from "../services/apiService";
+import { AiFillEdit, AiFillSave } from "react-icons/ai"; // Importing icons from react-icons
 import Footer from "./Footer";
 import "../css/DiaryRead.css";
+
 
 function DiaryRead() {
     const [diaryData, setDiaryData] = useState(null);
     const [isPublic, setIsPublic] = useState(false);
+    const [editMode, setEditMode] = useState(false);
+    const [editData, setEditData] = useState({ dream_name: "", dream: "" });
     const { diaryId } = useParams();
 
     useEffect(() => {
@@ -15,6 +19,7 @@ function DiaryRead() {
             console.log("Fetched diary:", data);
             setDiaryData(data);
             setIsPublic(data.data.is_public);
+            setEditData({ dream_name: data.data.dream_name, dream: data.data.dream });
         };
         fetchDiary();
     }, [diaryId]);
@@ -26,6 +31,24 @@ function DiaryRead() {
         await updatePublicStatus(diaryId, newPublicStatus);
     };
 
+    const handleEditClick = () => {
+        setEditMode(true);
+    };
+
+    const handleSaveClick = async () => {
+        setEditMode(false);
+        await editDiary(diaryId, editData);
+
+        // 수정된 다이어리를 다시 불러옵니다.
+        const updatedDiary = await getDiary(diaryId);
+        setDiaryData(updatedDiary);
+    };
+
+
+    const handleChange = (e) => {
+        setEditData({...editData, [e.target.name]: e.target.value});
+    };
+
     const getChecklistItems = (checklistStr) => {
         return checklistStr.split('\n');
     };
@@ -35,11 +58,26 @@ function DiaryRead() {
             {diaryData ? (
                 <>
                     <div className="header">
-                        <h2>{diaryData.data.dream_name}</h2>
+                        {diaryData.data.is_owner && (
+                            <button className="edit-button" onClick={editMode ? handleSaveClick : handleEditClick}>
+                                {editMode ? < AiFillSave /> : <AiFillEdit />}
+                            </button>
+                        )}
+                        {editMode ? (
+                            <input type="text" name="dream_name" className="edit-input" value={editData.dream_name} onChange={handleChange} />
+                        ) : (
+                            <h2>{diaryData.data.dream_name}</h2>
+                        )}
+
                         <hr />
                     </div>
                     <div className="image-container">
                         <img src={diaryData.data.image_url} alt={diaryData.data.dream_name} />
+                    </div>
+
+                    <div className="icons">
+                        <span className="like">❤️ {diaryData.data.like_count}</span>
+                        <span className="view">👁️ {diaryData.data.view_count}</span>
                         {diaryData.data.is_owner && (
                             <div className="public-status-container">
                                 <label className="switch">
@@ -55,13 +93,13 @@ function DiaryRead() {
                             </div>
                         )}
                     </div>
-                    <div className="icons">
-                        <span className="like">❤️ {diaryData.data.like_count}</span>
-                        <span className="view">👁️ {diaryData.data.view_count}</span>
-                    </div>
                     <div className="content">
                         {!isPublic && <p className="private-diary">비공개 게시물 입니다.</p>}
-                        <p className="dream">{diaryData.data.dream}</p>
+                        {editMode ? (
+                            <textarea className="save-input" name="dream" value={editData.dream} onChange={handleChange} />
+                        ) : (
+                            <p className="dream">{diaryData.data.dream}</p>
+                        )}
                         <p className="resolution">{diaryData.data.resolution}</p>
                         <ol className="checklist">
                             {getChecklistItems(diaryData.data.checklist).map((item, index) => (
@@ -73,9 +111,15 @@ function DiaryRead() {
             ) : (
                 <p>Loading...</p>
             )}
+            {editMode && (
+                <button className="save-button" onClick={handleSaveClick}>
+                    <AiFillSave /> Save
+                </button>
+            )}
             <Footer />
         </div>
     );
 }
 
 export default DiaryRead;
+

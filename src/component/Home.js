a2d5768b5c95ef80b598d45bb3823cab4a6b2265
Junbeetime <sgwag597@gmail.com>
2023-05-27 19:58:23 +0900

    feat: 꿈 로딩이팩트 추가, 편집 기능 추가

--- src/component/Home.js
+++ src/component/Home.js
@@ -27,33 +27,38 @@ const HomePage = () => {
             <header className="header">
                 <img src={logo} alt="Logo" className="logo"/>
                 <div className="button-group">
-                    <button className="button" onClick={() => handleButtonClick("/preRegister")}>
-                        사전등록하기
+                    <button className="button_register" onClick={() => handleButtonClick("/preRegister")}>
+                        사전 등록
                     </button>
                 </div>
             </header>
             <div className="content">
                 <div className="section" id="introduction">
-                    <p>"꿈은 무의식의 창이다."</p>
-                    <p>당신의 아침을 함께할 어플, 도슨트입니다.</p>
-                    <p>오늘 꾼 꿈을 잊어버리지 않게</p>
-                    <p>음성을 통해 기록하고, 꿈을 해몽하고, 그림을 그려보세요.</p>
+                    <p className="p">꿈 기록을 쉽고 재밌게</p>
+                    <p className="p">당신의 아침을 함께할,</p>
+                    <p className="p">도슨트입니다.</p>
+                    <button className="button" onClick={() => handleButtonClick("/createDream")}>
+                        체험하기
+                    </button>
+
+                    {/*<div className="background-video">*/}
+                    {/*    <video className="video" autoPlay loop muted>*/}
+                    {/*        <source src="../image/jellyVideo.mp4" type="video/mp4"/>*/}
+                    {/*    </video>*/}
+                    {/*</div>*/}
                 </div>
-                <img src={main} alt="main" data-aos="fade-up"/>
-                <button className="button" onClick={() => handleButtonClick("/experience")}>
-                    체험하기
-                </button>
+                <img className="mainImage" src={main} alt="main" data-aos="fade-up"/>
                 <img src={flow1} alt="Flow 1" data-aos="fade-up"/>
                 <img src={flow2} alt="Flow 2" data-aos="fade-up"/>
                 <img src={flow3} alt="Flow 3" data-aos="fade-up"/>
-                <div className="section" id="ourTeam">
+                <div className="section1" id="ourTeam">
                     <p>저희 창의성을 추구하고, 꿈을 사랑하는 사람들입니다.</p>
                     <p>개인의 창의성이 중요해지는 현대 사회,</p>
                     <p>우리는 문득 이런 생각을 했습니다.</p>
                     <p>"재미있는 꿈을 오래오래 기억할 수는 없을까?"</p>
                     <p>"꿈 속 장면이 그림으로 그려지면 좋겠다"</p>
                 </div>
-                <div className="section" id="ourService">
+                <div className="section2" id="ourService">
                     <p>도슨트는 당신의 꿈을 음성으로 기록해주고,</p>
                     <p>그 꿈을 해몽해드립니다.</p>
                     <p>당신이 무엇을 상상하고 있는지,</p>
@@ -62,7 +67,7 @@ const HomePage = () => {
                     <p>더불어 당신의 꿈을 그려볼 수 있는 도구인</p>
                     <p>그림을 제공합니다.</p>
                 </div>
-                <div className="section" id="ourVision">
+                <div className="section2" id="ourVision">
                     <p>도슨트는 당신의 이야기를 그림으로 그려드릴게요.</p>
                     <p>자신만의 방식으로 꿈을 기록하고 기억해보세요.</p>
                     <p>기록이 누적 될수록 자신의 내면에 더 가까워 질 수 있습니다.</p>

2c5000df1eb39680161512ea7b5906b24893f0c2
taewan <taewan2002@naver.com>
2023-07-18 18:38:52 +0900

    fix: 테스트 웹버전 디자인

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -124,7 +124,7 @@ export const getDiary = async (diaryId) => {
 export async function kakaoLogin() {
     try {
         // TODO: test용으로 임시로 작성한 코드 /test 제외 후 빌드 필요
-        const response = await fetch("/api/auth/kakao/test", {
+        const response = await fetch("/api/auth/kakao", {
             method: "POST",
             headers: {
                 Accept: "application/json",
@@ -147,7 +147,7 @@ export async function getAccessToken(code) {
     try {
         // TODO: test용으로 임시로 작성한 코드 /test 제외 후 빌드 필요
         console.log("code: ", code);
-        const response = await fetch(`/api/auth/kakao/callback/test?code=${code}`, {
+        const response = await fetch(`/api/auth/kakao/callback?code=${code}`, {
             method: "GET",
             headers: {
                 Accept: "application/json",
